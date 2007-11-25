32d8e19d756b5167e7081acf9c9f054bbb3d859c
(no author) <(no author)@93b75dfe-da2d-4241-a08d-2141769abc04>
2007-11-25 06:46:56 +0000

    This commit was manufactured by cvs2svn to create tag 'r_0_1'.
    git-svn-id: svn+ssh://140.110.240.196/partclone_svn@5 93b75dfe-da2d-4241-a08d-2141769abc04

---
+++ tags/r_0_1/src/partclone.h
@@ -0,0 +1,93 @@
+/**
+ * partclone.h - Part of Partclone project.
+ *
+ * Copyright (c) 2007~ Thomas Tsai <thomas at nchc org tw>
+ *
+ * function and structure used by main.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <malloc.h>
+#include <stdarg.h>
+#include <getopt.h>
+
+#define IMAGE_MAGIC "partclone-image"
+#define IMAGE_MAGIC_SIZE 15
+
+char *EXECNAME;
+
+/**
+ * option
+ * structure smd_opt
+ * usage	    - print message "how to use this"
+ * parse_options    - get parameter from agrc, argv
+ */
+struct cmd_opt
+{
+        int clone;
+        int restore;
+//	int dd;
+	int debug;
+        char* source;
+        char* target;
+};
+typedef struct cmd_opt cmd_opt;
+extern void usage(void);
+extern void parse_options(int argc, char **argv, cmd_opt* opt);
+
+/**
+ * debug message
+ * open_log	- to open file /var/log/partclone.log
+ * log_mesg	- write log to the file
+ *		- write log and exit 
+ *		- write to stderr...
+ */
+extern void open_log();
+extern void log_mesg(int lerrno, int lexit, int only_debug, int debug, const char *fmt, ...);
+extern void close_log();
+
+
+/**
+ * for restore used functions
+ * restore_image_hdr	- get image_head from image file 
+ * get_image_bitmap	- read bitmap data from image file
+ */
+struct image_head 
+{
+    char magic[IMAGE_MAGIC_SIZE];
+    int block_size;
+    unsigned long long device_size;
+    unsigned long long totalblock;
+    unsigned long long usedblocks;
+};
+typedef struct image_head image_head;
+
+extern void restore_image_hdr(int* ret, cmd_opt* opt, image_head* image_hdr);
+extern void get_image_hdr(int* ret, cmd_opt opt, image_head image_hdr, char* bitmap);
+
+/**
+ * for open and close
+ * open_source	- open device or image or stdin
+ * open_target	- open device or image or stdout
+ *
+ *	the data string 
+ *	clone:	    read from device to image/stdout
+ *	restore:    read from image/stdin to device
+ *	dd:	    read from device to device !! not complete
+ *
+ */
+extern int open_source(char* source, cmd_opt* opt);
+extern int open_target(char* target, cmd_opt* opt);
+
+/// print image_head
+extern void print_image_hdr_info(image_head image_hdr, cmd_opt opt);
+
---
+++ tags/r_0_1/src/xfsclone.h
@@ -0,0 +1,35 @@
+/**
+ * xfsclone.h - part of Partclone project
+ *
+ * Copyright (c) 2007~ Thomas Tsai <thomas at nchc org tw>
+ *
+ * read xfs super block and bitmap
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
+#include <xfs/libxfs.h>
+
+/// update bitmap table
+static void addToHist(int dwAgNo, int dwAgBlockNo, int qwLen, char* bitmap);
+
+/// scan bno
+static void scanfunc_bno(xfs_btree_sblock_t* ablock,  int level, xfs_agf_t* agf, char* bitmap);
+
+/// scan btree
+static void scan_sbtree(xfs_agf_t* agf, xfs_agblock_t root, int nlevels, char* bitmap);
+
+/// open device
+static void fs_open(char* device);
+
+/// close device
+static void fs_close();
+
+///  readbitmap - read bitmap
+extern void readbitmap(char* device, image_head image_hdr, char* bitmap);
+
+/// read super block and write to image head
+extern void initial_image_hdr(char* device, image_head* image_hdr);
