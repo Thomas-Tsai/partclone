4cec5a98714dbf4901a2c120e8bcae61541baa23
jazz <jazz@93b75dfe-da2d-4241-a08d-2141769abc04>
2008-01-02 14:04:03 +0000

    * These template files is not necessary for current packaging usage.
    git-svn-id: svn+ssh://140.110.240.196/partclone_svn@33 93b75dfe-da2d-4241-a08d-2141769abc04

--- trunk/debian/cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the partclone package
-#
-0 4	* * *	root	partclone_maintenance
--- trunk/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(partclone):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="partclone" command="/usr/bin/partclone"
