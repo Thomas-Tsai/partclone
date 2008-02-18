185433109e76fadbaba682274f8f05ed3138851e
thomas <thomas@93b75dfe-da2d-4241-a08d-2141769abc04>
2008-02-18 08:44:03 +0000

    add manual for clone.fat
    git-svn-id: svn+ssh://140.110.240.196/partclone_svn@111 93b75dfe-da2d-4241-a08d-2141769abc04

--- partclone/src/version.c
+++ partclone/src/version.c
@@ -3,9 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-<<<<<<< .mine
-const char svn_version[] = "Rev:107:108M";
-=======
-const char svn_version[] = "Rev:108M";
->>>>>>> .r109
+const char svn_version[] = "Rev:107:110M";
 
