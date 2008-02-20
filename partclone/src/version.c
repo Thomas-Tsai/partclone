377769f2a7751690d76187229754e681de070d1f
c00jhs00 <c00jhs00@93b75dfe-da2d-4241-a08d-2141769abc04>
2008-02-20 14:47:43 +0000

    clone.fat was not compiled.
    git-svn-id: svn+ssh://140.110.240.196/partclone_svn@113 93b75dfe-da2d-4241-a08d-2141769abc04

--- partclone/src/version.c
+++ partclone/src/version.c
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-const char svn_version[] = "Rev:107:110M";
+const char svn_version[] = "Rev:111:112M";
 
