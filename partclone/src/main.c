508e215124f05235f6811b82b1981a80b62493ef
thomas <thomas@93b75dfe-da2d-4241-a08d-2141769abc04>
2008-05-19 06:18:19 +0000

    * accept ticket #33, and update configure, *am, *in files.     * make all clone tools can restore any image which is created by partclone.
    git-svn-id: svn+ssh://140.110.240.196/partclone_svn@140 93b75dfe-da2d-4241-a08d-2141769abc04

--- partclone/src/main.c
+++ partclone/src/main.c
@@ -238,8 +238,8 @@ int main(int argc, char **argv){
 	    log_mesg(0, 1, 1, debug, "This is not partclone image.\n");
 
 	/// check the file system
-	if (strcmp(image_hdr.fs, FS) != 0)
-	    log_mesg(0, 1, 1, debug, "%s can't restore from the image which filesystem is %s not %s\n", argv[0], image_hdr.fs, FS);
+	//if (strcmp(image_hdr.fs, FS) != 0)
+	//    log_mesg(0, 1, 1, debug, "%s can't restore from the image which filesystem is %s not %s\n", argv[0], image_hdr.fs, FS);
 
 	log_mesg(2, 0, 0, debug, "initial main bitmap pointer %lli\n", bitmap);
 	log_mesg(1, 0, 0, debug, "Initial image hdr - read bitmap table\n");
