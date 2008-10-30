e2584c69748f63bdf9113d94d47a147870ad40ba
thomas <thomas@93b75dfe-da2d-4241-a08d-2141769abc04>
2008-10-30 09:01:38 +0000

    update for old libntfs(9) at debian etch
    git-svn-id: svn+ssh://140.110.240.196/partclone_svn@173 93b75dfe-da2d-4241-a08d-2141769abc04

--- partclone/src/ntfsclone-ng.c
+++ partclone/src/ntfsclone-ng.c
@@ -35,6 +35,14 @@
 #include "partclone.h"
 #include "reiserfsclone.h"
 
+//define mount flag
+#ifdef NTFS_MNT_RDONLY
+    #define LIBNTFS_VER_10 1
+#else
+    #define NTFS_MNT_RDONLY 1
+    #define LIBNTFS_VER_9 1
+#endif
+
 char *EXECNAME	= "partclone.ntfs";
 int debug	= 2;
 ntfs_volume *ntfs;
@@ -50,7 +58,7 @@ static void fs_open(char* device){
 	err = errno;
 	log_mesg(0, 1, 1, debug, "ntfs mount error %i\n", err);
     } else {
-	if (NVolWasDirty(ntfs)) {
+	if (ntfs->flags & VOLUME_IS_DIRTY) {
 	    log_mesg(0, 1, 1, debug, "NTFS Volume '%s' is scheduled for a check or it was shutdown\nuncleanly. Please boot Windows or use the --force option to progress.\n", device);
 	} else {
 	    log_mesg(3, 0, 0, debug, "NTFS Volume '%s' is clean\n", device);
@@ -143,7 +151,10 @@ extern void initial_image_hdr(char* device, image_head* image_hdr)
     memcpy(image_hdr->fs, ntfs_MAGIC, FS_MAGIC_SIZE);
     image_hdr->block_size  = (int)ntfs->cluster_size;
     image_hdr->totalblock  = (unsigned long long)ntfs->nr_clusters;
-    image_hdr->usedblocks  = (unsigned long long)(ntfs->nr_clusters - ntfs->nr_free_clusters - 1);
+
+#ifdef LIBNTFS_VER_10
+        image_hdr->usedblocks  = (unsigned long long)(ntfs->nr_clusters - ntfs->nr_free_clusters - 1);
+#endif
     image_hdr->device_size = (unsigned long long)ntfs_device_size_get(ntfs->dev, 1);
     fs_close();
 }
