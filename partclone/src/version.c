a73037cef39c85835644b250ff9a3680b8ac0de0
steven <steven@93b75dfe-da2d-4241-a08d-2141769abc04>
2009-04-16 07:19:04 +0000

    www.partclone.org was changed to partclone.org.
    git-svn-id: svn+ssh://140.110.240.196/partclone_svn@227 93b75dfe-da2d-4241-a08d-2141769abc04

--- partclone/src/version.c
+++ partclone/src/version.c
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-const char svn_version[] = "Rev:223M";
+const char svn_version[] = "Rev:225M";
 
