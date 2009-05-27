e0c3aede84f36c42ea7f945e469a4786e2137b29
steven <steven@93b75dfe-da2d-4241-a08d-2141769abc04>
2009-05-27 05:06:23 +0000

    Updated for version 0.1.1-4.
    git-svn-id: svn+ssh://140.110.240.196/partclone_svn@271 93b75dfe-da2d-4241-a08d-2141769abc04

--- partclone/src/version.c
+++ partclone/src/version.c
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-const char svn_version[] = "Rev:267M";
+const char svn_version[] = "Rev:270M";
 
