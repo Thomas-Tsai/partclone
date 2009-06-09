b9e00db6be31e630a90fba3d0ce741b1e0d10766
thomas <thomas@93b75dfe-da2d-4241-a08d-2141769abc04>
2009-06-09 07:56:49 +0000

    try to fix hfsplus problem-1 	modified:   src/hfsplusclone.c
    git-svn-id: svn+ssh://140.110.240.196/partclone_svn@289 93b75dfe-da2d-4241-a08d-2141769abc04

--- partclone/src/hfsplusclone.c
+++ partclone/src/hfsplusclone.c
@@ -1,5 +1,5 @@
 /**
- hfsplusclone.c - part of Partclone project
+  hfsplusclone.c - part of Partclone project
  *
  * Copyright (c) 2007~ Thomas Tsai <thomas at nchc org tw>
  *
@@ -53,12 +53,12 @@ static int reverseInt(int i){
 
 static int IsAllocationBlockUsed(UInt32 thisAllocationBlock, UInt8* allocationFileContents)
 {
-        UInt8 thisByte;
-	int debug = 1;
+    UInt8 thisByte;
+    int debug = 1;
 
-        thisByte = allocationFileContents[thisAllocationBlock / 8];
-	//log_mesg(0, 0, 0, debug, "IsAB:%i\n", (thisByte & (1 << (7 - (thisAllocationBlock % 8)))));
-        return (thisByte & (1 << (7 - (thisAllocationBlock % 8)))) != 0;
+    thisByte = allocationFileContents[thisAllocationBlock / 8];
+    //log_mesg(0, 0, 0, debug, "IsAB:%i\n", (thisByte & (1 << (7 - (thisAllocationBlock % 8)))));
+    return (thisByte & (1 << (7 - (thisAllocationBlock % 8)))) != 0;
 }
 
 static void print_fork_data(HFSPlusForkData* fork){
@@ -104,9 +104,9 @@ static void fs_open(char* device){
     log_mesg(3, 0, 0, debug, "Attr-Inconsistent=%i\n", (reverseInt(sb.attributes)>>11) & 1);
 
     if (HFS_Clean)
-	log_mesg(3, 0, 0, debug, "HFS_Plus '%s' is clean\n", device);
+        log_mesg(3, 0, 0, debug, "HFS_Plus '%s' is clean\n", device);
     else 
-	log_mesg(0, 1, 1, debug, "HFS_Plus Volume '%s' is scheduled for a check or it was shutdown\nuncleanly. Please fix it by fsck.\n", device);
+        log_mesg(0, 1, 1, debug, "HFS_Plus Volume '%s' is scheduled for a check or it was shutdown\nuncleanly. Please fix it by fsck.\n", device);
 
     free(buffer);
 
@@ -127,47 +127,61 @@ extern void readbitmap(char* device, image_head image_hdr, char* bitmap, int pui
     int debug = 2;
     int start = 0;
     int bit_size = 1;
+    int allocation_exten = 0;
+    int allocation_start_block;
+    int allocation_block_size;
+    int exten_bitmap = 0;
 
 
     fs_open(device);
     tb = reverseInt((int)sb.totalBlocks);
     rb = (tb/8)+1;
-    //rb = 8192;
 
     /// init progress
     progress_bar   prog;	/// progress_bar structure defined in progress.h
     progress_init(&prog, start, image_hdr.totalblock, bit_size);
 
     for (i = 0; i < tb; i++)
-	bitmap[i] = 1;
+        bitmap[i] = 1;
 
-    s = lseek(ret, 2560, SEEK_CUR);
-    buffer2 = (UInt8*)malloc(rb);
-    r = read (ret, buffer2, rb);
-    for(b = 0 ; b < tb; b++){
-	int check_block = b;
-	IsUsed = IsAllocationBlockUsed(check_block, buffer2);
-	if (IsUsed){
-	    bused++;
-	    bitmap[b] = 1;
-	    log_mesg(3, 0, 0, debug, "used b = %i\n", b);
-	}else{
-	    bfree++;
-	    bitmap[b] = 0;
-	    log_mesg(3, 0, 0, debug, "free b = %i\n", b);
-	}
-	/// update progress
-	update_pui(&prog, b, 0);
+    for (allocation_exten = 0; allocation_exten <= 7; allocation_exten++){
+        allocation_start_block = 4096*reverseInt(sb.allocationFile.extents[allocation_exten].startBlock);
 
+        allocation_block_size = 4096*reverseInt(sb.allocationFile.extents[allocation_exten].blockCount);
+        if((allocation_start_block == 0) && (allocation_block_size == 0)){
+            continue;
+        }
+
+        s = lseek(ret, allocation_start_block, SEEK_SET);
+        rb = allocation_block_size;
+        buffer2 = (UInt8*)malloc(rb);
+        r = read(ret, buffer2, rb);
+        for(b = exten_bitmap ; (b < allocation_block_size*8) && (b < tb); b++){
+            int check_block = b;
+            IsUsed = IsAllocationBlockUsed(check_block, buffer2);
+            if (IsUsed){
+                bused++;
+                bitmap[b] = 1;
+                log_mesg(3, 0, 0, debug, "used b = %i\n", b);
+            } else {
+                bfree++;
+                bitmap[b] = 0;
+                log_mesg(3, 0, 0, debug, "free b = %i\n", b);
+            }
+            exten_bitmap = allocation_block_size*8;
+            /// update progress
+            update_pui(&prog, b, 0);
+
+        }
+        free(buffer2);
+        log_mesg(2, 0, 0, 2, "buffer2:%i\n", buffer2);
+
+        log_mesg(2, 0, 0, 2, "bfree:%i\n", bfree);
+        log_mesg(2, 0, 0, 2, "bused:%i\n", bused);
     }
+    if(bused != (reverseInt(sb.totalBlocks) - reverseInt(sb.freeBlocks)))
+        log_mesg(0, 1, 1, debug, "bitmap count error, used:%i\n", bused);
 
-    log_mesg(2, 0, 0, 1, "rb:%i\n", rb);
-    log_mesg(2, 0, 0, 1, "bfree:%i\n", bfree);
-    log_mesg(2, 0, 0, 1, "bused:%i\n", bused);
-    if(bfree != reverseInt((int)sb.freeBlocks))
-        log_mesg(0, 1, 1, debug, "bitmap free count err, free:%i\n", bfree);
-
-    free(buffer2);
     fs_close();
     /// update progress
     update_pui(&prog, 1, 1);
