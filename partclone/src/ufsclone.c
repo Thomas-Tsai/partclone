aa654dadf9bab7d048f767a5fa2754036b63f71f
thomas <thomas@93b75dfe-da2d-4241-a08d-2141769abc04>
2009-09-03 08:34:24 +0000

    update automake andd ufsclone
    git-svn-id: svn+ssh://140.110.240.196/partclone_svn@315 93b75dfe-da2d-4241-a08d-2141769abc04

--- partclone/src/ufsclone.c
+++ partclone/src/ufsclone.c
@@ -41,43 +41,47 @@ char *EXECNAME = "partclone.ufs";
 
 /// open device
 static void fs_open(char* device){
+    int debug = 3;
 
+    log_mesg(3, 0, 0, debug, "UFS partition Open\n");
     if (ufs_disk_fillout(&disk, device) == -1){
-	printf("open error\n");
+        log_mesg(3, 0, 0, debug, "UFS open fail\n");
     }
+    log_mesg(3, 0, 0, debug, "UFS open well\n");
 
     switch (disk.d_ufs) {
-	case 2:
-	    printf("magic = %x (UFS2)\n", afs.fs_magic);
-	    printf("superblock location = %lld\nid = [ %x %x ]\n", afs.fs_sblockloc, afs.fs_id[0], afs.fs_id[1]);
-	    printf("group (ncg) = %d\n", afs.fs_ncg);
-	    printf("UFS size (tblocks) = %lld\n", afs.fs_dsize);
-	    printf("Blocksize fs_fsize = %i\n", afs.fs_fsize);
-	    printf("partition size = %lld\n", afs.fs_dsize*afs.fs_fsize);
-	    printf("block per group %i\n", afs.fs_fpg);
-	    break;
-	case 1:
-	    printf("magic = %x (UFS1)\n", afs.fs_magic);
-	    printf("id = [ %x %x ]\n", afs.fs_id[0], afs.fs_id[1]);
-	    break;
-	default:
-	    printf("disk.d_ufs = %c", disk.d_ufs);
-	    break;
+        case 2:
+            log_mesg(3, 0, 0, debug, "magic = %x (UFS2)\n", afs.fs_magic);
+            log_mesg(3, 0, 0, debug, "superblock location = %lld\nid = [ %x %x ]\n", afs.fs_sblockloc, afs.fs_id[0], afs.fs_id[1]);
+            log_mesg(3, 0, 0, debug, "group (ncg) = %d\n", afs.fs_ncg);
+            log_mesg(3, 0, 0, debug, "UFS size (tblocks) = %lld\n", afs.fs_dsize);
+            log_mesg(3, 0, 0, debug, "Blocksize fs_fsize = %i\n", afs.fs_fsize);
+            log_mesg(3, 0, 0, debug, "partition size = %lld\n", afs.fs_dsize*afs.fs_fsize);
+            log_mesg(3, 0, 0, debug, "block per group %i\n", afs.fs_fpg);
+            break;
+        case 1:
+            log_mesg(3, 0, 0, debug, "magic = %x (UFS1)\n", afs.fs_magic);
+            log_mesg(3, 0, 0, debug, "id = [ %x %x ]\n", afs.fs_id[0], afs.fs_id[1]);
+            break;
+        default:
+            log_mesg(3, 0, 0, debug, "disk.d_ufs = %c", disk.d_ufs);
+            break;
     }
 
 }
 
 /// close device
 static void fs_close(){
+    log_mesg(3, 0, 0, debug, "close\n");
     ufs_disk_close(&disk);
+    log_mesg(3, 0, 0, debug, "done\n\n");
 }
 
 /// readbitmap - read bitmap
 extern void readbitmap(char* device, image_head image_hdr, char* bitmap, int pui)
 {
-    reiser4_bitmap_t       *fs_bitmap;
-    unsigned long long     bit, block, bused = 0, bfree = 0;
-    int                    debug = 2;
+    unsigned long long     block, bused = 0, bfree = 0;
+    int                    debug = 3, done = 0;
 
     fs_open(device);
 
@@ -87,30 +91,29 @@ extern void readbitmap(char* device, image_head image_hdr, char* bitmap, int pui
 
     /// read group
     while ((i = cgread(&disk)) != 0) {
-	printf("\ncg = %d\n", disk.d_lcg);
-	printf("blocks = %i\n", acg.cg_ndblk);
-	p = cg_blksfree(&acg);
+        log_mesg(3, 0, 0, debug, "\ncg = %d\n", disk.d_lcg);
+        log_mesg(3, 0, 0, debug, "blocks = %i\n", acg.cg_ndblk);
+        p = cg_blksfree(&acg);
 
-	for (count = block = 0; block < afs.fs_fpg; block++){
-	    if (isset(p, block)) {
-		/*
-		if (count)
-		    printf(",%s", count % 6 ? " " : "\n\t");
-		count++;
-		printf("%d", block);
-		orig_block = block;
-		while ((block+1)<afs.fs_fpg && isset(p, block+1))
-		    block++;
-		if (block != orig_block)
-		    printf("-%d", block);
-		*/
-	    }
-	}
-	printf("\n");
+        for (block = 0; block < afs.fs_fpg; block++){
+            if (isset(p, block)) {
+                bitmap[block] = 1;
+                log_mesg(3, 0, 0, debug, "bitmap is used %lli", block);
+            } else {
+                bitmap[block] = 0;
+                bfree++;
+                log_mesg(3, 0, 0, debug, "bitmap is free %lli", block);
+            }
+            update_pui(&bprog, block ,done);
+        }
+        log_mesg(3, 0, 0, debug, "\n");
 
     }
 
     fs_close();
+    
+    done = 1;
+    update_pui(&bprog, 1, done);
 
 }
 
@@ -118,15 +121,13 @@ extern void readbitmap(char* device, image_head image_hdr, char* bitmap, int pui
 extern void initial_image_hdr(char* device, image_head* image_hdr)
 {
     int                    debug=1;
-    unsigned long long free_blocks=0;
 
     fs_open(device);
-    free_blocks = ;
     memcpy(image_hdr->magic, IMAGE_MAGIC, IMAGE_MAGIC_SIZE);
     memcpy(image_hdr->fs, ufs_MAGIC, FS_MAGIC_SIZE);
     image_hdr->block_size  = afs.fs_fsize;
     image_hdr->totalblock  = afs.fs_dsize;
-    image_hdr->usedblocks  = ;
+    image_hdr->usedblocks  = 0;
     image_hdr->device_size = afs.fs_fsize*afs.fs_dsize;
     fs_close();
 }
