7bd4d553ccef4fbe9a358bfcf8ef11bbb725414c
Thomas Tsai <thomas@nchc.org.tw>
2011-05-24 10:47:18 +0800

    release 0.2.4 with deb file

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "$Format:%H$"
+#define git_version  "53f169eb759faa64a950db176296630493de951f"
 
