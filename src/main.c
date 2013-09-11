2d0d80462a5ebb783a38d3c41d3dd3fd09f2fcfe
Patryck Rouleau <pfrouleau@gmail.com>
2013-09-11 19:48:41 -0400

    partclone.chkimg: check only the image's size when it does not contains checksums
    Signed-off-by: Patryck Rouleau <pfrouleau@gmail.com>

--- src/main.c
+++ src/main.c
@@ -520,6 +520,29 @@ int main(int argc, char **argv) {
 		free(write_buffer);
 		free(read_buffer);
 
+	// check only the size when the image does not contains checksums and does not
+	// comes from a pipe
+	} else if (opt.chkimg && img_opt.checksum_mode == CSM_NONE
+		&& strcmp(opt.source, "-") != 0) {
+
+		unsigned long long total_offset = (fs_info.usedblocks - 1) * fs_info.block_size;
+		char last_block[fs_info.block_size];
+		off_t partial_offset = INT32_MAX;
+
+		while (total_offset) {
+
+			if (partial_offset > total_offset)
+				partial_offset = total_offset;
+
+			if (lseek(dfr, partial_offset, SEEK_CUR) == (off_t)-1)
+				log_mesg(0, 1, 1, debug, "source seek ERROR: %s\n", strerror(errno));
+
+			total_offset -= partial_offset;
+		}
+
+		if (read_all(&dfr, last_block, fs_info.block_size, &opt) != fs_info.block_size)
+			log_mesg(0, 1, 1, debug, "ERROR: source image too short\n");
+
 	} else if (opt.restore) {
 
 		const unsigned long long blocks_total = fs_info.totalblock;
