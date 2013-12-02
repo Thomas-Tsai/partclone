21e49a2b66c43fc1f512bf3df77e4d8b22fcfc21
Thomas Tsai <thomas@nchc.org.tw>
2013-12-02 15:30:51 +0800

    update version and log

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "3abd4f2c0a7d1d8ce6ace014cb952757d10bdc0a"
+#define git_version  "bdfae06b0fc6700ba0ba44da6cbd7374c13f1f8e"
 
