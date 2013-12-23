3ee24c9682c745cdc7411ef8b0d78824ed955363
Thomas Tsai <thomas@nchc.org.tw>
2013-12-23 10:50:20 +0800

    merge partclone.c

--- src/version.h
+++ src/version.h
@@ -3,5 +3,9 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
+<<<<<<< HEAD
 #define git_version  "1faf626392ba4f4ee89cea86b59b6e595a4c04c4"
+=======
+#define git_version  "a9cb7ab754935210cd1bddfd17b5c5fb7ac85115"
+>>>>>>> c5651a8... fix segfault for info.c
 
