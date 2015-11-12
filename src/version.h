15e800f0fc759fea72d982e9a491cf8206bc5f74
Thomas Tsai <thomas@nchc.org.tw>
2015-11-12 14:04:18 +0800

    release 0.2.86

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "3c1a65bd6f44dc1575d5cd1010444bea0c670dcc"
+#define git_version  "ea8fa6072c21ce06e9bec08e0f692bd8195f9de4"
 
