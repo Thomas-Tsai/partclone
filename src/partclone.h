e5d0685cb74c157245ccef3a6766aa278d5b441b
tjjh89017 <tjjh89017@hotmail.com>
2019-01-05 20:44:52 +0800

    Add a flag for torrent only (need squash)

--- src/partclone.h
+++ src/partclone.h
@@ -112,6 +112,8 @@ struct cmd_opt
     int ignore_crc;
     int quiet;
     int blockfile;
+    /* torrent only flag */
+    int torrent;
     int no_block_detail;
     int restore_raw_file;
     int skip_write_error;
