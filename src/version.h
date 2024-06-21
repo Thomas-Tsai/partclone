910fb6422916af7bded494159ac0344db455ed26
Thomas Tsai <tlinux.tsai@gmail.com>
2024-06-21 10:20:38 +0800

    update git version generate by describe

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "1fc4ad9"
+#define git_version  "0.3.29-6-g4db7f48-dirty"
 
