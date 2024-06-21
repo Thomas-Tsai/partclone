00b53f7baa086c817e0adecff17f23fb3bb12a49
Thomas Tsai <tlinux.tsai@gmail.com>
2024-06-21 11:41:52 +0800

    release 0.3.30

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "0.3.29-6-g4db7f48-dirty"
+#define git_version  "0.3.29-12-ged0044d"
 
