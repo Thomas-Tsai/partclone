92fdb4a952ef6dd87118923a59509cfda3698485
Thomas Tsai <tlinux.tsai@gmail.com>
2024-12-10 09:32:03 +0800

    add f2fs 4kn sector size support

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "0.3.32-7-ge769821"
+#define git_version  "0.3.32-8-g79f27e2"
 
