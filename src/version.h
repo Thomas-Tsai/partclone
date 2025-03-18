f4213653eba9acee46e176707513138a14f0f536
Thomas Tsai <thomas@nchc.org.tw>
2025-03-18 08:18:28 +0800

    remove failmbr

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "0.3.33-10-g000c094"
+#define git_version  "0.3.33-13-g9ec3f61"
 
