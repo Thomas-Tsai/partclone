dd3c0f9754e373503716de9278439f676987aa18
Thomas Tsai <thomas@nchc.org.tw>
2025-03-19 09:48:51 +0800

    release 0.3.34

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "0.3.33-14-gf421365"
+#define git_version  "0.3.33-16-g2cc8bc4"
 
