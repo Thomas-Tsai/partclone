285099f91b08bd4ffe89e84ffb0c200d13c2537f
Thomas Tsai <tlinux.tsai@gmail.com>
2025-10-15 09:56:50 +0800

    release 0.3.38

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "0.3.37-8-g727fa4d"
+#define git_version  "0.3.37-10-gf18f2af"
 
