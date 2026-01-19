b762c9ffcc65dc06c9d8bb403cc8c46d636ea0f5
Thomas Tsai <tlinux.tsai@gmail.com>
2026-01-19 10:22:38 +0800

    update log and docs

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "0.3.40-22-gfac2388"
+#define git_version  "0.3.40-28-g6f94dad"
 
