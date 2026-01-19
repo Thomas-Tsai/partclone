6d70c9ef59b72684f1f17f277aae6aa39c5f4aeb
Thomas Tsai <tlinux.tsai@gmail.com>
2026-01-19 10:23:08 +0800

    release 0.3.41

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "0.3.40-28-g6f94dad"
+#define git_version  "0.3.40-29-gb762c9f"
 
