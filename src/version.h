d13e88f0d21cbdafa5045c1fe006344e9083b6e2
Thomas Tsai <tlinux.tsai@gmail.com>
2026-01-20 12:57:06 +0800

    update doc and remove .lo

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "0.3.40-29-gb762c9f"
+#define git_version  "0.3.41-1-g8f637ea"
 
