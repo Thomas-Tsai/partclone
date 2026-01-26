d30194c431d5db84cbc62daffd36538213013bda
Thomas Tsai <tlinux.tsai@gmail.com>
2026-01-26 13:08:41 +0800

    Fixing and upport both Static and Dynamic Builds add CI to verify dynamic and static build

--- src/btrfs_static_fixes.c
+++ src/btrfs_static_fixes.c
@@ -2,14 +2,14 @@
 #include <config.h>
 #endif
 
-#if !defined(HAVE_XXHASH)
-/* Scenario: xxhash disabled. We provide implementation (unprefixed) for btrfs. */
+#if defined(BTRFS_STATIC_BUILD)
+/* For static builds, always use prefix to avoid any conflict with system libs */
+#define XXH_NAMESPACE BTRFS_
 #define XXH_STATIC_LINKING_ONLY
 #define XXH_IMPLEMENTATION
 #include "btrfs/crypto/xxhash.h"
-#elif defined(BTRFS_STATIC_BUILD)
-/* Scenario: static build + enabled. We provide wrapped implementation to avoid conflict. */
-#define XXH_NAMESPACE BTRFS_
+#elif !defined(HAVE_XXHASH)
+/* Scenario: xxhash disabled and NOT a static build. We provide implementation (unprefixed) for btrfs. */
 #define XXH_STATIC_LINKING_ONLY
 #define XXH_IMPLEMENTATION
 #include "btrfs/crypto/xxhash.h"
