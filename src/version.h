f502de0c39c11d33638f2c24bcdd2df98e7c5781
Thomas Tsai <tlinux.tsai@gmail.com>
2026-03-03 16:49:59 +0800

    update tests for xxh128

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "0.3.45-3-gcef4a39"
+#define git_version  "0.3.45-4-g73f2341"
 
