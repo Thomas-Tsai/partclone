b287963556551fe25d0291a41b9e181d22e0f6a7
Thomas Tsai <tlinux.tsai@gmail.com>
2026-03-04 13:55:34 +0800

    Bump version to 0.3.46

--- src/version.h
+++ src/version.h
@@ -3,5 +3,5 @@
  * WHETHER THEY ARE BUILT BY OTHERS OR DURING DEVELOPMENT OR FOR THE
  * OFFICIAL PARTCLONE RELEASES.
  */
-#define git_version  "0.3.45-4-g73f2341"
+#define git_version  "6479b09"
 
